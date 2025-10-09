140a0f144ddbb991e2142456e3bb98193fd9f735
Vedant0402 <vedanthedau23@gmail.com>
2025-10-09 10:58:30 +0530

    Data Ingestion component implemented and error solved successfully

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,9 +1,11 @@
+
 from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 uri = "mongodb+srv://vedanthedau23:<password>@cluster0.rgeemjd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
-client = MongoClient(uri)
+client = MongoClient(uri, server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
 try:
